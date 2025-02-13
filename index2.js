7afadeac32cb2d46170c62cadf11297ec3fb7995
Syed Saqib <soharwardiandco27@gmail.com>
2025-02-13 23:09:42 +0530

    middleware practice

---
+++ index2.js
@@ -0,0 +1,10 @@
+// introducing another route that does kidney replacement 
+// Inputs needs to be the same
+
+const express = require("express")
+
+const app = express()
+
+app.get("/health-checkup", function(req, res){
+    
+})
